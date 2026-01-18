3c0614bae2b4458ad7f83221cbebc9c1eb0d460a
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-18 21:22:42 +0100

    feat: add async and sync SQLAlchemy engines with simple UNION query test

--- src/database.py
+++ src/database.py
@@ -1,5 +1,7 @@
-from sqlalchemy import URL, create_engine, text
+import asyncio
 
+from sqlalchemy import create_engine, text
+from sqlalchemy.ext.asyncio import create_async_engine
 from src.config import settings
 
 engine = create_engine(
@@ -9,7 +11,21 @@ engine = create_engine(
     max_overflow=10
 )
 
+async_engine = create_async_engine(
+    url=settings.database_url_asyncpg,
+    echo=True,
+    pool_size=5,
+    max_overflow=10
+)
 
-with engine.connect() as conn:
-    res = conn.execute(text("SELECT VERSION()"))
-    print(f"{res=}")
+
+#with engine.connect() as conn:
+    #res = conn.execute(text("SELECT 1,2,3 union select 4,5,6"))
+    #print(f"{res.first()=}")
+
+async def get_123():
+    async with async_engine.connect() as conn:
+        res = await conn.execute(text("SELECT 1,2,3 union select 4,5,6"))
+        print(f"{res.first()=}")
+
+asyncio.run(get_123())
