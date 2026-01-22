c344cfb0c86df5c0cfc60a12130a2f7a41d09851
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-22 20:08:51 +0100

    feat: make insert_data async using async_session_factory

--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,5 +1,5 @@
 ﻿from sqlalchemy import text, insert
-from src.database import async_engine, sync_engine, session_factory
+from src.database import async_engine, sync_engine, session_factory, async_session_factory
 from src.models import metadata_obj, workers_table, WorkersORM
 
 
@@ -15,3 +15,10 @@ def insert_data():
         worker_zebra = WorkersORM(username="Zebra")
         session.add_all([worker_bobr, worker_zebra])
         session.commit()
+
+async def insert_data():
+   async with async_session_factory() as session:
+        worker_bobr = WorkersORM(username="Bobr")
+        worker_zebra = WorkersORM(username="Zebra")
+        session.add_all([worker_bobr, worker_zebra])
+        await session.commit()
