e65e729e61da63a92eaf64bb6c45c0b8ede1ac43
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-22 19:49:02 +0100

    fix: correct import path for Base in models.py from database to src.database refactor: add tablename = "workers" explicitly in WorkersORM class

--- src/models.py
+++ src/models.py
@@ -3,10 +3,11 @@
 from sqlalchemy import Table, String, Integer, Column, MetaData
 from sqlalchemy.orm import Mapped, mapped_column
 
-from database import Base
+from src.database import Base
 
 
 class WorkersORM(Base):
+    __tablename__ = "workers"
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column()
 
