506477389ac3e7f0d4bc43293e5508614606f59f
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-22 19:44:51 +0100

    feat: add insert_data function with example WorkersORM instances
    feat: implement insert of Bobr and Zebra workers via session.add_all

--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,7 +1,6 @@
 ﻿from sqlalchemy import text, insert
-from src.database import async_engine, sync_engine
-from src.models import metadata_obj, workers_table
-
+from src.database import async_engine, sync_engine, session_factory
+from src.models import metadata_obj, workers_table, WorkersORM
 
 
 def create_tables():
@@ -10,3 +9,9 @@ def create_tables():
     metadata_obj.create_all(sync_engine)
     sync_engine.echo = True
 
+def insert_data():
+    with session_factory() as session:
+        worker_bobr = WorkersORM(username="Bobr")
+        worker_zebra = WorkersORM(username="Zebra")
+        session.add_all([worker_bobr, worker_zebra])
+        session.commit()
