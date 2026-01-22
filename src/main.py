39ff414c1708ea0d00fcd23a6dad66b8707cd826
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-22 19:48:20 +0100

    feat: change import path from src.queries.core to src.queries.orm in main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-from src.queries.core import create_tables, insert_data
+#from src.queries.core import create_tables, insert_data
+from src.queries.orm import create_tables, insert_data
 
 create_tables()
 insert_data()
