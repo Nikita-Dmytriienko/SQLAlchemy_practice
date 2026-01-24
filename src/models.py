d753f8879209dbca2f191d29cb6c5ee2fc26d1be
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-24 16:59:28 +0100

    feat: add WorkLoad enum for parttime and fulltime workloads feat: add ResumesOrm model with id, title, compensation (optional), and workload fields

--- src/models.py
+++ src/models.py
@@ -1,9 +1,11 @@
-﻿from importlib.metadata import metadata
+﻿from typing import Optional
 
-from sqlalchemy import Table, String, Integer, Column, MetaData
+from sqlalchemy import Table, String, Integer, Column, MetaData, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database import Base
+
+import enum
 
 
 class WorkersORM(Base):
@@ -12,8 +14,19 @@ class WorkersORM(Base):
     username: Mapped[str] = mapped_column()
 
 
+class Workload(enum.Enum):
+    partime = "parttime"
+    fulltime = "fulltime"
 
 
+class ResumesOrm(Base):
+    __tablename__ = "resumes"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column()
+    compensation: Mapped[Optional[int]] = mapped_column(nullable=True)
+    workload: Mapped[Workload]
+    worker: Mapped[id] = mapped_column(ForeignKey=("workers.id"))
 
 
 
