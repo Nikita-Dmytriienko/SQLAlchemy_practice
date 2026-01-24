915cbd415a18062f376f9e4ca19f12b1270f076e
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-24 17:54:47 +0100

    chore: remove unused imports (text, insert, async_engine, session_factory, metadata_obj, workers_table) chore: remove commented out synchronous insert_data function

--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,20 +1,13 @@
-﻿from sqlalchemy import text, insert
-from src.database import async_engine, sync_engine, session_factory, async_session_factory
-from src.models import metadata_obj, workers_table, WorkersORM
+﻿from src.database import sync_engine, async_session_factory, Base
+from src.models import WorkersORM
 
 
 def create_tables():
     sync_engine.echo = False
-    metadata_obj.drop_all(sync_engine)
-    metadata_obj.create_all(sync_engine)
+    Base.metadata.drop_all(sync_engine)
+    Base.metadata.create_all(sync_engine)
     sync_engine.echo = True
 
-# def insert_data():
-#     with session_factory() as session:
-#         worker_bobr = WorkersORM(username="Bobr")
-#         worker_zebra = WorkersORM(username="Zebra")
-#         session.add_all([worker_bobr, worker_zebra])
-#         session.commit()
 
 async def insert_data():
    async with async_session_factory() as session:
