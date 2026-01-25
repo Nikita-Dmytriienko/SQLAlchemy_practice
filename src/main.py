de7853c981b1172826705b310e48e9c56bf6a39b
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-25 11:14:58 +0100

    feat: switch main.py to use SyncORM and SyncCore classes for execution
    refactor: replace direct calls to create_tables/insert_data with class method invocations
    
    chore: update import from src.queries.orm to import SyncORM, AsyncORM
    
    chore: update import from src.queries.core to import SyncCore
    
    chore: comment out async execution and old asyncio.run(insert_data())
    
    chore: comment out redundant AsyncORM.create_tables() / insert_workers() calls
    
    chore: add calls to SyncCore.select_workers() and SyncCore.update_workers()
    
    style: remove unnecessary import asyncio (no longer used)

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
-﻿import asyncio
-import os
+﻿import os
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-#from src.queries.core import create_tables, insert_data
-from src.queries.orm import create_tables, insert_data
+from src.queries.core import SyncCore
+from src.queries.orm import SyncORM, AsyncORM
 
-create_tables()
+SyncORM.create_tables()
+SyncORM.insert_workers()
+#SyncCore.create_tables()
 
-asyncio.run(insert_data())
+#AsyncORM.create_tables()
+#AsyncORM.insert_workers()
+#AsyncCore.create_tables()
+
+SyncCore.select_workers()
+SyncCore.update_workers()
+
+# asyncio.run(insert_data())
