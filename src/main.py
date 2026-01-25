c5755acf14d5abe59dd2214a1cdcb00c704a6a40
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-25 13:14:45 +0100

    feat: expand main execution with SyncORM insert_resumes and select_resumes_avg_compensation
    chore: reorder imports alphabetically and add noqa: F401 for unused imports
    
    style: move sys.path.insert after imports for cleaner structure

--- src/main.py
+++ src/main.py
@@ -1,25 +1,30 @@
-﻿import os
+import os
 import sys
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-from src.queries.core import SyncCore, AsyncCore
-from src.queries.orm import SyncORM, AsyncORM
+from src.queries.core import AsyncCore, SyncCore  # noqa: F401
+from src.queries.orm import AsyncORM, SyncORM  # noqa: F401
+
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+
 
 SyncORM.create_tables()
 SyncORM.insert_workers()
 SyncORM.select_workers()
 SyncORM.update_worker()
 
+SyncORM.insert_resumes()
+SyncORM.select_resumes_avg_compensation()
+
 # SyncCore.create_tables()
 # SyncCore.insert_workers()
 # SyncCore.select_workers()
 # SyncCore.update_worker()
-
+#
 # AsyncORM.create_tables()
 # AsyncORM.insert_workers()
 # AsyncORM.select_workers()
 # AsyncORM.update_worker()
-
+#
 # AsyncCore.create_tables()
 # AsyncCore.insert_workers()
 # AsyncCore.select_workers()
