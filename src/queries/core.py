030b3a2eea481f9979e444fd56f4c7b524091dba
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-25 10:26:40 +0100

    feat: introduce SyncCore class with static methods for core sync operations feat: add AsyncCore class with async create_tables using run_sync for metadata ops refactor: move create_tables logic into SyncCore and AsyncCore classes refactor: add insert_workers static method to SyncCore using core insert chore: import select from sqlalchemy (even if unused yet) chore: remove standalone create_tables and insert_data functions chore: comment out old select_workers example in SyncCore style: fix duplicate create_tables definition in SyncCore (keep one)

--- src/queries/core.py
+++ src/queries/core.py
@@ -1,4 +1,4 @@
-﻿from sqlalchemy import text, insert
+﻿from sqlalchemy import text, insert, select
 from src.database import async_engine, sync_engine
 from src.models import metadata_obj, workers_table
 
@@ -13,25 +13,51 @@ async def get_123():
         res = await conn.execute(text("SELECT 1,2,3 union select 4,5,6"))
         print(f"{res.first()=}")
 
+#Sync version
+class SyncCore:
+    @staticmethod
+    def create_tables():
+        sync_engine.echo = False
+        metadata_obj.drop_all(sync_engine)
+        metadata_obj.create_all(sync_engine)
+        sync_engine.echo = True
 
-def create_tables():
-    sync_engine.echo = False
-    metadata_obj.drop_all(sync_engine)
-    metadata_obj.create_all(sync_engine)
-    sync_engine.echo = True
+    @staticmethod
+    def create_tables():
+        sync_engine.echo = False
+        metadata_obj.drop_all(sync_engine)
+        metadata_obj.create_all(sync_engine)
+        sync_engine.echo = True
 
+    def insert_workers():
+        with sync_engine.connect() as conn:
+            stmt = insert(workers_table).values(
+                [
+                    {"username": 'Vasya'},
+                    {"username": 'Max'},
+                ]
+            )
+            conn.execute(stmt)
+            conn.commit()
 
-def insert_data():
-    with sync_engine.connect() as conn:
-        # stmt = """INSERT INTO workers (username) VALUES
-        #         ('Boba'),
-        #         ('Roma'),
-        #         ('Vitya');"""
-        stmt = insert(workers_table).values(
-            [
-                {"username": 'Vasya'},
-                {"username": 'Max'},
-            ]
-        )
-        conn.execute(stmt)
-        conn.commit()
+    # @staticmethod
+    # def select_workers():
+    #     with session_factory() as session:
+    #         query = select(WorkersOrm)
+    #         result = session.execute(query)
+    #         workers = result.scalars().all()
+
+#Async  version
+class AsyncCore:
+    @staticmethod
+    async def create_tables():
+        async with async_engine.begin() as conn:
+            await conn.run_sync(metadata_obj.drop_all)
+            await conn.run_sync(metadata_obj.create_all)
+
+        # @staticmethod
+        # def create_tables():
+        #     sync_engine.echo = False
+        #     metadata_obj.drop_all(sync_engine)
+        #     metadata_obj.create_all(sync_engine)
+        #     sync_engine.echo = True
