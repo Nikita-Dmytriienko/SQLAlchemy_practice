780ddb87d90cdb62fd3b92a9a64ce17c7d51cca7
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2026-01-28 14:30:17 +0100

    refactor(main): restructure CLI execution with async/sync mode selection and FastAPI integration

--- src/main.py
+++ src/main.py
@@ -1,32 +1,59 @@
+import asyncio
 import os
 import sys
 
-from src.queries.core import AsyncCore, SyncCore  # noqa: F401
-from src.queries.orm import AsyncORM, SyncORM  # noqa: F401
+import uvicorn
+from fastapi import FastAPI
+
+from src.queries.core import AsyncCore, SyncCore
+from src.queries.orm import AsyncORM, SyncORM
 
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
 
 
-SyncORM.create_tables()
-SyncORM.insert_workers()
-SyncORM.select_workers()
-SyncORM.update_worker()
+async def main():
+    # ========== SYNC ==========
+    # ORM
+    if "--orm" in sys.argv and "--sync" in sys.argv:
+        SyncORM.create_tables()
+        SyncORM.insert_workers()
+        SyncORM.select_workers()
+        SyncORM.update_worker()
 
-SyncORM.insert_resumes()
-SyncORM.insert_additional_resumes()
-SyncORM.select_resumes_avg_compensation()
+        SyncORM.insert_resumes()
+        SyncORM.insert_additional_resumes()
+        SyncORM.select_resumes_avg_compensation()
 
-# SyncCore.create_tables()
-# SyncCore.insert_workers()
-# SyncCore.select_workers()
-# SyncCore.update_worker()
-#
-# AsyncORM.create_tables()
-# AsyncORM.insert_workers()
-# AsyncORM.select_workers()
-# AsyncORM.update_worker()
-#
-# AsyncCore.create_tables()
-# AsyncCore.insert_workers()
-# AsyncCore.select_workers()
-# AsyncCore.update_worker()
+        # CORE
+    elif "--core" in sys.argv and "--sync" in sys.argv:
+        SyncCore.create_tables()
+        SyncCore.insert_workers()
+        SyncCore.select_workers()
+        SyncCore.update_worker()
+
+    # ========== ASYNC ==========
+    # ORM
+    if "--orm" in sys.argv and "--async" in sys.argv:
+        AsyncORM.create_tables()
+        AsyncORM.insert_workers()
+        AsyncORM.select_workers()
+        AsyncORM.update_worker()
+
+    # CORE
+    elif "--core" in sys.argv and "--async" in sys.argv:
+        AsyncCore.create_tables()
+        AsyncCore.insert_workers()
+        AsyncCore.select_workers()
+        AsyncCore.update_worker()
+
+
+app = FastAPI()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    if "--webserver" in sys.argv:
+        uvicorn.run(
+            app="src.main:app",
+            reload=True,
+        )
