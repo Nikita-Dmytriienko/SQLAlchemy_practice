e7a278878beb6ab7d98de5feaa98861dccbb724e
Nikita <91425615+Nikita-Dmytriienko@users.noreply.github.com>
2026-01-28 13:14:22 +0100

    Update test_data.sql

--- test_data.sql
+++ test_data.sql
@@ -2,3 +2,17 @@
 ('Boba'),
 ('Roma'),
 ('Vitya');
+
+select * from resumes;
+
+select *, compensation-avg_workload_compensation as compensation_diff
+from
+(select
+	w.id,
+	w.username,
+	r.compensation,
+	r.workload,
+	avg(r.compensation) over (partition by workload) ::int as avg_workload_compensation	
+	from resumes r
+	join workers w on r.worker_id = w.id) helper1
+
